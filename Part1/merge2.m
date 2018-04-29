595e8d980b86e9daf5fab2c67da66ebe1ba4df67
JonasBraun0411 <jonasmjbraun@googlemail.com>
2018-04-29 21:19:56 +0100

    last version

--- Part1/merge2.m
+++ Part1/merge2.m
@@ -1,49 +1,36 @@
-function [res3] =  merge2()
-%     skip = 1;
-%     sampling = 'uniform';
-%     samples = 5000;
-%     iterations = 30;
-%     rms = 0.00001;
-%     verbose = 1;
-%     method = 'bruteforce';
-%     res = merge_all31(skip, sampling, samples, iterations,rms,verbose,method);
-%     save('ours_default1.mat','res');
-    
-%     skip = 2;
-%     sampling = 'uniform';
-%     samples = 5000;
-%     iterations = 30;
-%     rms = 0.00001;
-%     verbose = 1;
-%     method = 'bruteforce';
-%     res2 = merge_all31(skip, sampling, samples, iterations,rms,verbose,method);
-%     save('ours_default2.mat','res2');
-    
-    skip = 1;
-    sampling = 'uniform';
-    samples = 5000;
-    iterations = 20;
-    rms = 0.00001;
-    verbose = 2;
-    method = 'knn';
-    res3 = merge_all32(skip, sampling, samples, iterations,rms,verbose,method);
-    save('ours_default3.mat','res');
-    
-end
+%call merge2() to gridsearch approaches 3.1 and 3.2 with specified params
+function merge2()
 
-function gridsearch()
-    
-for sk = 1:6
-   sp = [1,2,4,6,8,10];
+%choose desired parameters to run ICP on
+%bruteforce or knn
+method = 'knn';
+verbose = 1;
+%cutoff RMS delta between 2 iterations
+rms = 0.0001;
+%skip in-between frames
+sp = [1,2,4,6,8,10];
+%number of samples
+sa = [500,1000,2000,5000,10000,20000];
+%number of max ICP iterations unless rms is unchanged
+it = [10,20,30];
+%choose sampling method
+sampling = 'uniform';
+%only used for comp. intense lower density 3.2 improvement
+%uncomment code below and set distance to desired radius
+distance = 0.01;
+
+%loops over all parameter settings
+for sk = 1:size(sp,2);
    skip = sp(sk);
-   sampling = 0;
-   for s = 1:5
-       sa = [500,1000,2000,5000,10000];
+   for s = 1:size(sa,2);
        samples = sa(s);
-       for iterations = 10:10:30
+       for i = 1:size(it,2);
+           iterations = it(i);
+           
+           % build model according to 3.1. approach 
            try
-            res = merge_all31(skip, sampling, samples, iterations,0.0001,0);
-            save(sprintf('31__%d_%d_%d.mat',skip,samples,iterations),'res');
+            res = merge_all31(skip, sampling, samples, iterations,rms,verbose,method);
+            save(sprintf([sampling,'_',method,'_','31_%d_%d_%d.mat'],skip,samples,iterations),'res');
            catch ME
                    getReport(ME)
                 'exception'
@@ -51,9 +38,10 @@ for sk = 1:6
                 samples
                 iterations
            end
+           % build model according to 3.2. approach
            try
-            res = merge_all32(skip, sampling, samples, iterations,0.0001,0);
-            save(sprintf('32__%d_%d_%d.mat',skip,samples,iterations),'res');
+            res = merge_all32(skip, sampling, samples, iterations,rms,verbose,method);
+            save(sprintf([sampling,'_',method,'_','32_%d_%d_%d.mat'],skip,samples,iterations),'res');
            catch ME
                     getReport(ME)
                 'exception'
@@ -61,42 +49,43 @@ for sk = 1:6
                 samples
                 iterations
            end
-           try
-
-            res = merge_all32_improved(skip, sampling, samples, iterations,0.01,0.0001,0);
-            save(sprintf('32imp__%d_%d_%d.mat',skip,samples,iterations),'res');
-           catch ME
-                 getReport(ME)
-                'exception'
-                skip
-                samples
-                iterations
-           end
+           %uncomment below for 3.2 improvement, takes a very long time
+           
+%            try
+% 
+%             res = merge_all32_improved(n, sampling, samples,...
+%             iterations,distance,rms, verbose);
+%             save(sprintf([sampling,'_',method,'_','imp_%d_%d_%d.mat'],skip,samples,iterations),'res');
+%            catch ME
+%                  getReport(ME)
+%                 'exception'
+%                 skip
+%                 samples
+%                 iterations
+%            end
 
        end
    end
 end
 end
 
-
+%load pointcloud files
 function pc = get_pointcloud(number)
     pc = readPcd(sprintf('Data/data/%010d.pcd', number));
-    %dismiss 4th dimension
     pc = pc(:,1:3);
     %omit points further away than 2m
     pc = pc(pc(:,3)<2,:);
 end
 
-
+% as specified in 3.1, compute transformation from consecutive frames
 function res = merge_all31(n, sampling, samples, iterations,rms,verbose,method)
     merged = get_pointcloud(0);
-%     EE = zeros(floor(100/n), iterations + 1);
     for i = n:n:99
+        i
         base = get_pointcloud(i-n);
         target = get_pointcloud(i);
         [R,t] = ICP2(base,target,'sampling',sampling,'samples',samples,'max_iter',...
             iterations,'rms',rms,'verbose',verbose,'method',method);
-%        EE( i/n,:) = E';
         for j=1:size(merged,1)
             merged(j,:) = (R*merged(j,1:3)' + t )';
         end
@@ -104,16 +93,17 @@ function res = merge_all31(n, sampling, samples, iterations,rms,verbose,method)
     end
     res = merged;
 end
-
+% as specified in 3.2, compute transformation from whole merged model and
+% next frame
 function res = merge_all32(n, sampling, samples, iterations,rms,verbose,method)
     p = get_pointcloud(0);
-%     EE = zeros(floor(100/n), iterations + 1);
-    
+
     for i = n:n:99
+        i
         q = get_pointcloud(i);
         [R,t] = ICP2(p,q,'sampling',sampling,'samples',samples,'max_iter',...
             iterations,'rms',rms,'verbose',verbose,'method',method);
-%        EE( i/n,:) = E';
+
         for j=1:size(p,1)
             p(j,:) = (R*p(j,1:3)' + t )';
         end
@@ -124,15 +114,14 @@ end
 
 % remove points too close to another
 function res = merge_all32_improved(n, sampling, samples, iterations,distance,rms, verbose)
-%     EE = zeros(floor(100/n), iterations + 1);
 
     p = get_pointcloud(0);
 
     for i = n:n:99
+        i
         q = get_pointcloud(i);
         [R,t] = ICP2(p,q,'sampling',sampling,'samples',samples,'max_iter',...
             iterations,'rms',rms,'verbose',verbose,'method',method);
-%        EE( i/n,:) = E';
         for j=1:size(p,1)
             p(j,:) = (R*p(j,1:3)' + t )';
         end
@@ -142,23 +131,18 @@ function res = merge_all32_improved(n, sampling, samples, iterations,distance,rm
     while(j>1) 
         
         pp = p(j,:);
-        %size(res,1)
         p = [ p( sqrt(  sum(   (p-pp).^2,2)   )>distance ,:)   ;pp];
-        %size(x,1)
-        
+
         if size(p,1)<j ; j = size(p,1); else; j=j-1; end
     end
     
     
     end  
-        
-
-    
     res = p;
 end
 
 
-
+% plot model with colour of data points as depth
 function plot_res(res)
 %blue
 blue = zeros(size(res,1),1);
@@ -175,7 +159,4 @@ red = zeros(size(res,1),1);
 red(1,1)=1;
 
 fscatter3(res(:,1),res(:,2),res(:,3),res(:,3));
-% fscatter3(res(:,1),res(:,2),res(:,3),res(:,3));
-% fscatter3(pc1(:,1),pc1(:,2),pc1(:,3),blue);
-% fscatter3(pc2(:,1),pc2(:,2),pc2(:,3),green);
 end
