e4f80e7f9dcde7906b7c4ab9de1780704d9cbe6d
robertkajnak <hezirisz@gmail.com>
2018-05-07 13:37:02 +0200

    The for loop now takes the first file twice per instructions

--- Part2/Main.m
+++ Part2/Main.m
@@ -1,19 +1,23 @@
 %% Add path variables and clear workspace
 addpath(genpath('./Assignment_4'))
 clear all; close all;
-%% Load files
+%% Preparing file names
 path = 'data/House/';
 
 files = dir(path);
-M = length(files)-2;
+%The initial files is taken twice (1->2 and end->1)
+M = length(files)-1;
 images = cell(1,M);
 for i=3:length(files)
     images{i-2} = files(i).name;
 end
+images{end} = files(1).name;
 
 %TODO - eliminate points from background - how?
 %% Load through all files
 for i=1:M-1
+    fprintf('Preparing matches between %d<->%d',i,i+1);
+    %% Load image pairs
     im1name = sprintf([path,images{i}],i);
     im2name = sprintf([path,images{i+1}],i+1);
     im1 = imread(im1name);
